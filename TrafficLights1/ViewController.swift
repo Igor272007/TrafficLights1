f24f2ee1dbf59b3668a0348202a69bb280c7bd13
Kuznetcov Igor <kuznetcov28@mail.ru>
2023-06-19 23:47:04 +0300

    Initial Commit

---
+++ TrafficLights1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLights1
+//
+//  Created by Игорь Кузнецов on 6/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
